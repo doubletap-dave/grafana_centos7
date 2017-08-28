6a3998e7c2a65f2a5e619355711a972f15790b05
topiaryx <davidvad3r@icloud.com>
2017-08-28 10:41:50 -0600

    Updating format, modularizing

---
+++ functions_a.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#### File: functions.sh
+#### Version: 1.5a
+#### Description: Functions needed in order to install Docker, Grafana, InfluxDB and Graphite
+#### Author: Dave (/u/topiaryx) - topiaryx@gmail.com - 28 August 2017 - Utah
+
+# Check for root permissions
+
+
+# Check machine or VM IP address
+
+
+# Prereq installer
+
+
+# DOCKER: Remove old Docker installations
+
+
+# DOCKER: Add Docker repo
+
+
+# DOCKER: Install Docker
+
+
+# DOCKER: Run and enable Docker
+
+
+# DOCKER: Verify Docker is running
+
+
